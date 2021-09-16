b09f13a3bfad41ee45a676e9302864d587acbff4
Tomoya Nakanishi <tomoya2517@gmail.com>
2021-09-17 00:24:16 +0900

    Fix: sending files before go build

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,10 @@
 
 # Common deploy script
 
-# rsync -avh ./webapp isu1:/home/isucon/
-# rsync -avh ./webapp isu2:/home/isucon/
-# rsync -avh ./webapp isu3:/home/isucon/
-
 # (cd ./webapp/go/ && go build -o isucondition)
 # cp /home/isucon/isucon11-quals/webapp/go/isucondition /home/isucon/webapp/go/
 # scp /home/isucon/isucon11-quals/webapp/go/isucondition 192.168.0.12:/home/isucon/webapp/go/
+
+# rsync -avh ./webapp isu1:/home/isucon/
+# rsync -avh ./webapp isu2:/home/isucon/
+# rsync -avh ./webapp isu3:/home/isucon/
