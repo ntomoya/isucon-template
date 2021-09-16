8ca4aa530eb720d5518f2eb0f7190df331bfccfb
Tomoya Nakanishi <tomoya2517@gmail.com>
2021-09-16 15:29:29 +0900

    Change build method for go

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,6 @@
 # rsync -avh ./webapp isu2:/home/isucon/
 # rsync -avh ./webapp isu3:/home/isucon/
 
-# (cd ./webapp/go/ && make)
+# (cd ./webapp/go/ && go build -o isucondition)
 # cp /home/isucon/isucon11-quals/webapp/go/isucondition /home/isucon/webapp/go/
 # scp /home/isucon/isucon11-quals/webapp/go/isucondition 192.168.0.12:/home/isucon/webapp/go/
